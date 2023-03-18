f874d90b4f6f2731c3c0e239f3fd0b9b2dc47781
Eric 张哲涵 <67270559+AntDock@users.noreply.github.com>
2023-03-18 13:57:18 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -102,7 +102,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
             </p>
           ) : (
             <p className="ml-2 font-medium text-day dark:text-night header-name">
-              {BLOG.title}{' '}{' '}|{' '}{' '}
+              {BLOG.title}：
               <span className="font-normal">{BLOG.description}</span>
             </p>
           )}
