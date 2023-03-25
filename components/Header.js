b2624b77865e92372bedc543c2e7b533052ff7c0
Eric 张哲涵 <67270559+AntDock@users.noreply.github.com>
2023-03-24 19:36:42 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,8 @@ import { useLocale } from '@/lib/locale'
 const NavBar = () => {
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: "朋友圈", to: 'https://s.antdock.cn', show: true },
+    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: false },
+    { id: 1, name: "时刻", to: 'https://s.antdock.cn', show: true },
     { id: 2, name: locale.NAV.ABOUT, to: '/about-me', show: BLOG.showAbout },
     { id: 3, name: locale.NAV.RSS, to: '/feed', show: false },
     { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
