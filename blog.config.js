7c12d11ae5ee9679ca7025df48d696bdbe168c9e
Eric 张哲涵 <67270559+AntDock@users.noreply.github.com>
2023-03-24 19:17:26 -0700

    Include TZ and change BG color in dark mode

--- blog.config.js
+++ blog.config.js
@@ -5,10 +5,11 @@ const BLOG = {
   link: 'https://antdock.cn',
   description: 'thinking different(ly) as always.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#18181B', // use hex value, don't forget '#'
+  darkBackground: '#000000', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2020, // If leave this empty, current year will be used.
   postsPerPage: 8,
