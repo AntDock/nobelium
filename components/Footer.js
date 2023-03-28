4f21a9fdb7e806107313226bae5ce2821ae193d1
Eric 张哲涵 <67270559+AntDock@users.noreply.github.com>
2023-03-28 21:02:56 +0800

    Add Service Status Link

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ const Footer = ({ fullWidth }) => {
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
           <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
+            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`} <a href="https://antdock.statuspage.io">服务状态</a>
           </p>
           <Vercel />
         </div>
